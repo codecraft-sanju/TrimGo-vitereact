1268bc3486e8ea2a083a71431808e0c46f767371
codecraft-sanju <sanjusirvich18@gmail.com>
2025-12-27 11:51:05 +0530

    vite config pwd add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa' // 1. Yeh import naya hai
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // 2. Yeh PWA ki setting add ki hai
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'TrimGo - Smart Salon Queue Management', 
+        short_name: 'Trimgo',
+        description: 'offline working',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
